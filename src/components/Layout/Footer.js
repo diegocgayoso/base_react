8a5ebd919a1dd4315b9e4d0c145a363d892ba2f5
diegocgayoso <diegoc.gayoso@gmail.com>
2021-12-05 12:06:51 -0300

    aplicando icones no footer com react-icon

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import styles from '../../styles/Footer.module.css';
+
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div>
                 <p className={styles.descriptionFooter}>
-                Aeroporto 7, 4005 - Industrial Teresina, Piauí PI - 57567-970.
-                emailexemplo@example.ts
+                    Aeroporto 7, 4005 - Industrial Teresina, Piauí PI - 57567-970.
+                    emailexemplo@example.ts
                 </p>
+                <div >
+                    <ul className={styles.socialMidia}>
+                        <li>
+                            <FaFacebook />
+                        </li>
+                        <li>
+                            <FaInstagram />
+                        </li>
+                        <li>
+                            <FaLinkedin />
+                        </li>
+                    </ul>
+                </div>
             </div>
-        
+
             <ul className={styles.listFooter}>
                 <li className={styles.itemLink}>
                     <Link to="/">Home</Link>
@@ -22,7 +37,7 @@ export default function Footer() {
                     <Link to="/contato">Contato</Link>
                 </li>
             </ul>
-        
+
 
         </footer>
     )
